6d5f5d3318c5713e670e391efd080bde04d0885d
Achala Punsara <achala.magallagoda@gmail.com>
2025-09-06 12:33:08 +0530

    change backend URL

--- src/util/Constants.jsx
+++ src/util/Constants.jsx
@@ -1,3 +1,4 @@
-export const APPURL = "http://localhost:8080/CeyBazaar/"
+// export const APPURL = "http://localhost:8080/CeyBazaar/"
+export const APPURL = "https://heartfelt-laughter-staging.up.railway.app/CeyBazaar/"
 
 export const SUCCESS = 1000
